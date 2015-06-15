85ba8b88866c3baae2e15db33d3b7aec2660d4c4
Tony <wukun522@gmail.com>
2015-06-15 18:41:21 +0800

    update deploy config

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,11 @@ lock '3.4.0'
 
 set :application, 'biu_server'
 set :repo_url, 'https://github.com/tonywu522/biu_server.git'
+set :rbenv_type, :user
 set :rbenv_ruby, '2.2.2'
+set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
+set :rbenv_map_bins, %w{rake gem bundle ruby rails}
+set :rbenv_roles, :all # default value
 
 # Default branch is :master
 ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
@@ -38,13 +42,26 @@ set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', '
 set :keep_releases, 5
 
 namespace :deploy do
-
-  after :restart, :clear_cache do
+    
+    desc "reload the database with seed data"
+    task :seed => [:set_rails_env] do
+        on primary fetch(:migration_role) do
+          within release_path do
+            with rails_env: fetch(:rails_env) do
+              execute :rake, "db:seed"
+            end
+          end
+        end
+     end
+     
+     after 'deploy:migrate', 'deploy:seed'
+    
+    after :restart, :clear_cache do
     on roles(:web), in: :groups, limit: 3, wait: 10 do
       # Here we can do anything such as:
       # within release_path do
       #   execute :rake, 'cache:clear'
       # end
     end
-  end
+    end
 end
---
+++ lib/capistrano/tasks/seeds.rb
@@ -0,0 +1,51 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+if (Zodiac.count == 0)
+    Zodiac.create(name: "Aries")
+    Zodiac.create(name: "Taurus")
+    Zodiac.create(name: "Gemini")
+    Zodiac.create(name: "Cancer")
+    Zodiac.create(name: "Leo")
+    Zodiac.create(name: "Virgo")
+    Zodiac.create(name: "Libra")
+    Zodiac.create(name: "Scorpio")
+    Zodiac.create(name: "Sagittarius")
+    Zodiac.create(name: "Capricorn")
+    Zodiac.create(name: "Aquarius")
+    Zodiac.create(name: "Pisces")
+end
+
+if (Style.count == 0)
+    Style.create(name: "Rich", gender:1)
+    Style.create(name: "GFS", gender:1)
+    Style.create(name: "DS", gender:1)
+    Style.create(name: "Talent", gender:1)
+    Style.create(name: "Soprt", gender:1)
+    Style.create(name: "Fashion", gender:1)
+    Style.create(name: "BigBoy", gender:1)
+    Style.create(name: "Common", gender:1)
+    Style.create(name: "All", gender:1)
+    Style.create(name: "Godness", gender:0)
+    Style.create(name: "BFM", gender:0)
+    Style.create(name: "DS", gender:0)
+    Style.create(name: "Talent", gender:0)
+    Style.create(name: "Soprt", gender:0)
+    Style.create(name: "Sexy", gender:0)
+    Style.create(name: "LovelyGirl", gender:0)
+    Style.create(name: "BusinessWoman", gender:0)
+    Style.create(name: "All", gender:0)
+end
+
+if (Sexuality.count == 0)
+    Sexuality.create(name: "Man")
+    Sexuality.create(name: "Woman")
+    Sexuality.create(name: "P")
+    Sexuality.create(name: "T")
+    Sexuality.create(name: "1")
+    Sexuality.create(name: "0")
+end
