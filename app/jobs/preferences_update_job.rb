2ff69c3e55e9a23f7b4c9dfba0207f256ed0f4b9
Tony <wukun522@gmail.com>
2015-08-20 09:46:56 +0800

    fix update profile failed problem

--- app/jobs/preferences_update_job.rb
+++ app/jobs/preferences_update_job.rb
@@ -3,6 +3,10 @@ class PreferencesUpdateJob < ActiveJob::Base
 
   def perform(user)
     # Do something later
+    if !user.partner || !user.profile
+        return
+    end
+    
     prefer_users = user.prefer_users - user.candidates
     if prefer_users.count > 0
         prefer_users.each do |prefer_matching_user|
