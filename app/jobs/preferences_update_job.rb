5ac6c2ebd807057359b6376975a63a0c3c41ec0c
Tony <wukun522@gmail.com>
2015-08-21 17:42:56 +0800

    fix perferences problem

--- app/jobs/preferences_update_job.rb
+++ app/jobs/preferences_update_job.rb
@@ -7,6 +7,10 @@ class PreferencesUpdateJob < ActiveJob::Base
         return
     end
     
+    if !user.prefer_users
+        Rails.logger.debug { "There is no prefer users for user: #{user.id} #{user.username}" }
+        return
+    end
     prefer_users = user.prefer_users - user.candidates
     if prefer_users.count > 0
         prefer_users.each do |prefer_matching_user|
